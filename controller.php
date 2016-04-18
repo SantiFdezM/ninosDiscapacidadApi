8209db62d9d778dbb25ba1ae074096d002bb3dad
Santiago Fernandez Madero <santi@Santiagos-MBP.lan>
2016-04-18 13:02:05 -0500

    Initial commit

---
+++ controller.php
@@ -0,0 +1,29 @@
+<?php 
+
+include 'functions.php';
+
+if(empty($_POST)){
+	echo 'error';
+	exit();
+}
+
+$funcion = $_POST['funcion'];
+
+switch ($funcion) {
+	case 'login':
+		//echo login($_POST['nombre_usuario'], $_POST['password'], $_POST['nombre_dispositivo'], $_POST['os'], $_POST['guid']);
+		break;
+	case 'logout':
+		//echo logout($_POST['token']);
+		break;
+	// case 'registro':
+	// 	echo registro($_POST['nombre'], $_POST['apellido'], $_POST['email'], $_POST['nombre_usuario'], $_POST['password'], $_POST['nombre_dispositivo'], $_POST['os'], $_POST['guid']);
+	// 	break;
+	default:
+		echo json_encode(0);
+		break;
+}
+
+
+
+?>
---
+++ functionsSession.php
@@ -0,0 +1,5 @@
+<?php 
+	/* Este archivo contiene las funciones que controlan la sesion de un usuario, login, logout, verifySession */
+
+	
+?>
